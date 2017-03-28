8bef3e7e275e4a72973efdc7ee8641e5d2821f13
Daniele Demichelis <d.demichelis@techgap.it>
2017-03-28 14:52:49 +0200

    Works!

--- src/main/java/com/danidemi/BTMWithH2Programmatically.java
+++ src/main/java/com/danidemi/BTMWithH2Programmatically.java
@@ -1,21 +1,24 @@
 package com.danidemi;
 
 import bitronix.tm.BitronixTransactionManager;
+import bitronix.tm.Configuration;
 import bitronix.tm.TransactionManagerServices;
+import bitronix.tm.resource.jdbc.PoolingDataSource;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.h2.jdbcx.JdbcDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Properties;
+import java.util.UUID;
 
 import static java.lang.String.format;
 
 /** Inspired by https://www.codeproject.com/Articles/143432/An-Introduction-to-Bitronix-JTA-Transaction-Manage */
-public class BTMWithH2 {
+public class BTMWithH2Programmatically {
 
     private static final String USER_NAME = "javauser";
     private static final String PASSWORD = "javadude";
@@ -23,30 +26,57 @@ public class BTMWithH2 {
     private static final String INSERT_QUERY = "insert into student(roll_number,name,class,section) values (?,?,?,?)";
     private static final String SELECT_QUERY = "select * from student";
 
-    private static final Logger log = LoggerFactory.getLogger(BTMWithH2.class);
+    private static final Logger log = LoggerFactory.getLogger(BTMWithH2Programmatically.class);
 
     public static void main(String[] args) {
-        new BTMWithH2().run();
+        new BTMWithH2Programmatically().run();
     }
 
     private void run() {
-        JdbcDataSource ds = new JdbcDataSource();
-        ds.setURL( format("jdbc:h2:%s/test", FileUtils.getTempDirectory().getAbsolutePath()) );
-        ds.setUser("sa");
-        ds.setPassword("sa");
+
+//        JdbcDataSource ds = new JdbcDataSource();
+//        ds.setURL( format("jdbc:h2:%s/test", FileUtils.getTempDirectory().getAbsolutePath()) );
+//        ds.setUser("sa");
+//        ds.setPassword("sa");
+
+
+
+
+        Configuration conf = TransactionManagerServices.getConfiguration();
+        conf.setServerId(UUID.randomUUID().toString());
+        conf.setResourceConfigurationFilename( getClass().getResource("/btm.properties").getFile() );
 
         BitronixTransactionManager btm = TransactionManagerServices.getTransactionManager();
+
+        PoolingDataSource ds = new PoolingDataSource();
+        ds.setUniqueName("code");
+        ds.setMaxPoolSize(10);
+        ds.setMinPoolSize(2);
+        ds.setClassName("org.h2.jdbcx.JdbcDataSource");
+        Properties driverProperties = new Properties();
+        driverProperties.setProperty("URL", format("jdbc:h2:%s\\test", FileUtils.getTempDirectory().getAbsolutePath()));
+        driverProperties.setProperty("user", "sa");
+        driverProperties.setProperty("password", "sa");
+        //driverProperties.setProperty("URL", format("jdbc:h2:%s\\test;USER=sa;PASSWORD=sa", FileUtils.getTempDirectory().getAbsolutePath()));
+        ds.setDriverProperties(driverProperties);
+        ds.init();
+
+
+        try {
+            btm.begin();
+            Connection createConnection = ds.getConnection();
+            createConnection.createStatement().execute(IOUtils.toString( BTMWithH2Programmatically.class.getResourceAsStream("/create_tables.sql") ));
+            createConnection.close();
+            btm.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         // a successful Bitronix transaction
         try		{
 
-            Connection createConnection = ds.getConnection(USER_NAME, PASSWORD);
-            createConnection.createStatement().execute(IOUtils.toString( BTMWithH2.class.getResourceAsStream("/create_tables.sql") ));
-            createConnection.close();
-
-
             btm.begin();
-            Connection connection = ds.getConnection(USER_NAME, PASSWORD);
+            Connection connection = ds.getConnection();
 
             PreparedStatement pstmt =
                     connection.prepareStatement(INSERT_QUERY);
@@ -79,7 +109,7 @@ public class BTMWithH2 {
 
             {
                 log.info("Inserting student in section B");
-                Connection connection = ds.getConnection(USER_NAME, PASSWORD);
+                Connection connection = ds.getConnection();
                 PreparedStatement pstmt =
                         connection.prepareStatement(INSERT_QUERY);
                 for (int index = 1; index <= 5; index++) {
@@ -95,7 +125,7 @@ public class BTMWithH2 {
 
             {
                 log.info("Inserting student in section C");
-                Connection connection = ds.getConnection(USER_NAME, PASSWORD);
+                Connection connection = ds.getConnection();
                 PreparedStatement pstmt =
                         connection.prepareStatement(INSERT_QUERY);
                 for (int index = 1; index <= 5; index++) {
@@ -126,7 +156,7 @@ public class BTMWithH2 {
         // a successful select Bitronix transaction
         try {
             btm.begin();
-            Connection connection = ds.getConnection(USER_NAME, PASSWORD);
+            Connection connection = ds.getConnection();
             PreparedStatement pstmt = connection.prepareStatement(SELECT_QUERY);
             ResultSet rs = pstmt.executeQuery();
             while(rs.next()){
